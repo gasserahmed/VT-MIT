d658f2e525232f2ee68e9fb16d9ff1010af1ef6b
gassermontaser <gassermontaser@gmail.com>
2020-03-19 00:23:16 -0400

    Finished P1M3 except for the description of data in P5.11C

--- ACIS5504/Projects/1-GCS/Milestone 3 Database Population and SQL DML/reports_query.sql
+++ ACIS5504/Projects/1-GCS/Milestone 3 Database Population and SQL DML/reports_query.sql
@@ -1,4 +1,4 @@
---Report #1: P5.11c
+--Report #1: P5.11C
 SELECT T_TaskDescription_TK AS `Project Task`,
 	   D_TaskStartDate_PS AS `Start Date`,
 	   D_TaskEndDate_PS AS `End Date`,
@@ -19,10 +19,27 @@ WHERE C_ProjectID_PS = 1
 ORDER BY C_TaskID_TK, C_AssignmentID_AS;
 ----------------------------------------------------------------------
 
---Report #2:
+--Report #2: P5.11A
+SELECT T_SkillDescription_SK AS `Skill`,
+	   T_FirstName_EP & ' ' & T_LastName_EP AS `Employee`
+FROM 
+(Skill s
+LEFT JOIN EmployeeSkill es on es.C_SkillID_ES = s.C_SkillID_SK)
+LEFT JOIN Employee e on e.C_EmployeeID_EP = es.C_EmployeeID_ES
+ORDER BY C_SkillID_SK;
 
 ----------------------------------------------------------------------
 
---Report #3:
-
+--Report #3: P5.11D
+SELECT T_FirstName_EP & ' ' & LEFT(T_LastName_EP, 1) & '.' AS `Employee Name`,
+	   D_WeekEndingDate_WL AS `Week Ending`,
+	   C_AssignmentID_WL AS `Assignment Number`,
+	   N_HoursWorked_WL AS `Hours Worked`,
+	   C_BillID_WL AS `Bill Number`
+FROM
+((WorkLog wl
+LEFT JOIN Assignment a on a.C_AssignmentID_AS = wl.C_AssignmentID_WL)
+LEFT JOIN Employee e on e.C_EmployeeID_EP = a.C_EmployeeID_AS)
+LEFT JOIN ProjectSchedule ps on ps.C_TaskID_PS = a.C_TaskID_AS
+WHERE C_ProjectID_PS = 1;
 ----------------------------------------------------------------------
