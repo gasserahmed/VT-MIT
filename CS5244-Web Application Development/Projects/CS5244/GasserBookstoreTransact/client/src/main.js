5ed09be29c5a60c26fc4ea921e207f30ae4f6f12
DESKTOP-LTRGDF2\Gasser Ahmed <gassermontaser@gmail.com>
2021-11-23 11:48:19 -0500

    Added credit icon next to payment methods's credit card number. Also added phone number formatter to be used in Ship To section

--- CS5244-Web Application Development/Projects/CS5244/GasserBookstoreTransact/client/src/main.js
+++ CS5244-Web Application Development/Projects/CS5244/GasserBookstoreTransact/client/src/main.js
@@ -40,6 +40,17 @@ Vue.filter("formatDisplayDate", function (displayDate) {
   return dateTime + timeZoneAbbreviation;
 });
 
+Vue.filter("formatPhoneNumber", function (phoneNumber) {
+  const cleaned = ("" + phoneNumber).replace(/\D/g, "");
+  const match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
+  if (match) {
+    var intlCode = match[1] ? "+1 " : "";
+    return [intlCode, "(", match[2], ") ", match[3], "-", match[4]].join("");
+  }
+
+  return phoneNumber;
+});
+
 Vue.use(Vuelidate);
 
 new Vue({
