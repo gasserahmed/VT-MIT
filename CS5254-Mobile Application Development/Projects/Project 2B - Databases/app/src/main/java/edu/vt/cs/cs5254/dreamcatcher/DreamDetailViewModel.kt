a058fb0e40ad014d95f6e28754bd54ee1c1a5086
Gasser Ahmed <gasser18@vt.edu>
2022-03-22 14:46:12 -0400

    Removed unused commented block from DreamDetailViewModel

--- CS5254-Mobile Application Development/Projects/Project 2B - Databases/app/src/main/java/edu/vt/cs/cs5254/dreamcatcher/DreamDetailViewModel.kt
+++ CS5254-Mobile Application Development/Projects/Project 2B - Databases/app/src/main/java/edu/vt/cs/cs5254/dreamcatcher/DreamDetailViewModel.kt
@@ -11,22 +11,6 @@ class DreamDetailViewModel : ViewModel() {
     private val dreamRepository = DreamRepository.get()
     private val dreamIdLiveData = MutableLiveData<UUID>()
 
-//    var dreamLiveData: LiveData<DreamWithEntries> =
-//        Transformations.switchMap(dreamIdLiveData) { dreamId ->
-//            dreamRepository.getDreamWithEntries(dreamId)
-//        }
-//
-//    fun loadDream(dreamId: UUID) {
-////        dreamWithEntries = dreamRepository.getDreamWithEntries(dreamId)
-////            ?: throw IllegalArgumentException("Dream with ID $dreamId not found")
-//        dreamIdLiveData.value = dreamId
-//            ?: throw IllegalArgumentException("Dream with ID $dreamId not found")
-//    }
-//
-//    fun saveDream(dreamWithEntries: DreamWithEntries) {
-//        dreamRepository.updateDreamWithEntries(dreamWithEntries)
-//    }
-
     var dreamLiveData: LiveData<DreamWithEntries> =
         Transformations.switchMap(dreamIdLiveData) { dreamId ->
             dreamRepository.getDreamWithEntries(dreamId)
