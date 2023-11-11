a1d35cbc8d74b5eb492bfe7c83d340de338b852b
Gasser Ahmed <gasser18@vt.edu>
2023-11-11 17:02:17 -0500

    Finished lab 4

--- ECE5984-SS Data Engineering Project/Assignments/4/Lab/load_db.py
+++ ECE5984-SS Data Engineering Project/Assignments/4/Lab/load_db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 import numpy as np
 from s3fs.core import S3FileSystem
 
@@ -6,7 +6,7 @@ from s3fs.core import S3FileSystem
 def load_data():
     s3 = S3FileSystem()
     # S3 bucket directory (data warehouse)
-    DIR_wh = ''  # Insert here
+    DIR_wh = 's3://ece5984-bucket-gasser18/Lab2/lab2-transform'  # Insert here
     # Get data from S3 bucket as a pickle file
     aapl_df = np.load(s3.open('{}/{}'.format(DIR_wh, 'clean_aapl.pkl')), allow_pickle=True)
     amzn_df = np.load(s3.open('{}/{}'.format(DIR_wh, 'clean_amzn.pkl')), allow_pickle=True)
@@ -18,15 +18,20 @@ def load_data():
                                    pw="admin12345",
                                    endpnt="database-dataeng.cwgvgleixj0c.us-east-1.rds.amazonaws.com"))
 
-    engine.execute("CREATE DATABASE {db}"
-                   .format(db=""))  # Insert pid here
+    # Check if the database already exists
+    inspector = inspect(engine)
+    if 'gasser18' not in inspector.get_schema_names():
+        # If the database doesn't exist, create it
+        engine.execute("CREATE DATABASE {db}"
+                       .format(db="gasser18"))  # Insert pid here
+    else:
+        print("Database 'gasser18' already exists")
 
     engine = create_engine("mysql+pymysql://{user}:{pw}@{endpnt}/{db}"
                            .format(user="admin",
                                    pw="admin12345",
                                    endpnt="database-dataeng.cwgvgleixj0c.us-east-1.rds.amazonaws.com",
-                                   db=""))
-
+                                   db="gasser18"))
 
     # Insert whole DataFrame into MySQL DB
     aapl_df.to_sql('aapl_clean',  # put table name here
